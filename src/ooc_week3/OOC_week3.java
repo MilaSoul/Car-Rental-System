1247285ed9c5f5afe6e8e7fd3b81b7aeb07d5ae5
snelbops <mila.dig@yahoo.com>
2021-10-07 18:08:43 +0100

    Initial commit

---
+++ src/ooc_week3/OOC_week3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ooc_week3;
+
+/**
+ *
+ * @author milastolbetskaya
+ */
+public class OOC_week3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello Beka");
+        
+    }
+    
+}
